f264230f1d174502621382ff139c2e7f53c4dd09
kyj <flysoar3029@gmail.com>
2022-11-12 21:23:58 +0900

    카테고리 목록 API에 데이터 요청 구현

--- app/src/main/java/com/shoppi/app/repository/home/HomeAssetDataSource.kt
+++ app/src/main/java/com/shoppi/app/repository/home/HomeAssetDataSource.kt
@@ -1,10 +1,11 @@
-package com.shoppi.app.repository
+package com.shoppi.app.repository.home
 
 import com.google.gson.Gson
 import com.shoppi.app.AssetLoader
 import com.shoppi.app.model.HomeData
 
 class HomeAssetDataSource(private val assetLoader: AssetLoader) : HomeDataSource {
+
     private val gson = Gson()
 
     override fun getHomeData(): HomeData? {
